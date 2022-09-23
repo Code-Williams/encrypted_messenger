c0e4bef48bb26d910ada6585cff604c2b6fe3de8
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-09-23 22:34:33 +0330

    New rank added

--- helpers/auth.js
+++ helpers/auth.js
@@ -6,7 +6,7 @@ const isUserLoggedIn = (req, res, next) => {
   if (!req.user) {
     req.flash("warning", "برای دیدن این صفحه وارد شوید");
     req.session.redirectTo = req.url;
-    return res.redirect("/login");
+    return res.redirect("/voroodi");
   }
 
   next();
@@ -18,50 +18,14 @@ const isUserNotLoggedIn = (req, res, next) => {
   next();
 };
 
-// ?----------- Chcek if user have moderator or super moderator rank on stream
-const isUserMod = async (req, res, next) => {
-  if (!req.user) {
-    return res.status(403).render("error", config.errors[403]);
-  }
-
-  const findUser = await User.findOne({
-    where: {
-      username: req.user.username,
-    },
-  });
-
-  if (
-    findUser &&
-    (findUser.userRank == "moderator" ||
-      findUser.userRank == "super mod" ||
-      findUser.userRank == "developer" ||
-      findUser.userRank == "owner")
-  ) {
-    next();
-  } else {
-    return res.status(403).render("error", config.errors[403]);
-  }
-};
-
-const isUserSuperMod = async (req, res, next) => {
-  if (!req.user) {
-    return res.status(403).render("error", config.errors[403]);
-  }
-
-  if (
-    req.user.userRank == "developer" ||
-    req.user.userRank == "super moderator" ||
-    req.user.userRank == "owner"
-  ) {
-    next();
-  } else {
-    return res.status(403).render("error", config.errors[403]);
-  }
+const isUserAdmin = (req, res, next) => {
+  if (req.user.userRank.toLowerCase() != "admin")
+    return res.redirect("/dashboard");
+  next();
 };
 
 module.exports = {
   isUserLoggedIn,
   isUserNotLoggedIn,
-  isUserMod,
-  isUserSuperMod,
+  isUserAdmin,
 };
