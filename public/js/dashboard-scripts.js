e02c170a9fb1cd6015c7d1922868b5d49b4f17b4
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-09-23 22:37:12 +0330

    password switcher added

--- public/js/dashboard-scripts.js
+++ public/js/dashboard-scripts.js
@@ -3,3 +3,11 @@ if (messages[1]) {
   var target = document.getElementById(messages[messages.length - 1].id);
   target.scrollIntoView();
 }
+
+var encryptBtn = document.getElementById("encryptMsgBtn");
+var passwordInp = document.getElementById("passInp");
+encryptBtn.addEventListener("click", () => {
+  encryptBtn.checked
+    ? (passwordInp.style.display = "inline-block")
+    : (passwordInp.style.display = "none");
+});
