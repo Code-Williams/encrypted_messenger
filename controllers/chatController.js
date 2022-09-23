7aea15cc234bc16f8ea30cc80bbaa2482378ce96
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-09-23 22:36:05 +0330

    Password support and clean code

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,36 +3,26 @@ const User = require("../models/User");
 const Message = require("../models/Message");
 
 const get = async (req, res) => {
-  const messages = await Message.findAll({
+  let messages = await Message.findAll({
     where: {
       chat_id: req.params.id,
     },
   });
-  const firstDMList = await DMList.findAll({
+  let firstDMList = await DMList.findAll({
     where: {
       creator_id: req.user.id,
     },
   });
 
-  const secondDMList = await DMList.findAll({
+  let secondDMList = await DMList.findAll({
     where: {
       chatter_id: req.user.id,
     },
   });
 
-  if (req.params.id == "new") {
-    res.render("dashboard", {
-      user: req.user,
-      flash: req.flash(),
-      firstDMList,
-      secondDMList,
-      messages: null,
-      chatId: 0,
-      mode: "new chat",
-    });
-
-    return;
-  }
+  let chatId = req.params.id == "new" ? 0 : req.params.id;
+  let mode = req.params.id == "new" ? "new chat" : "chat messages";
+  if (req.params.id == "new") messages = null;
 
   res.render("dashboard", {
     user: req.user,
@@ -40,55 +30,21 @@ const get = async (req, res) => {
     firstDMList,
     secondDMList,
     messages,
-    chatId: req.params.id,
-    encrypt: Message.encrypt,
-    mode: "chat messages",
+    chatId,
+    mode,
   });
 };
 
 const post = async (req, res) => {
-  if (req.body.id) {
-    if (req.body.id !== req.user.username) {
-      const isUserValid = await User.findOne({
-        where: {
-          username: req.body.id,
-        },
-      });
+  if (!req.body.id) {
+    const dmMessages = await Message.findAll({
+      where: {
+        chat_id: req.params.id,
+      },
+    });
 
-      if (isUserValid) {
-        const firstDMList = await DMList.findOne({
-          where: {
-            creator_id: req.user.id,
-            chatter_id: isUserValid.id,
-          },
-        });
+    if (dmMessages[29]) dmMessages[0].destroy();
 
-        const secondDMList = await DMList.findOne({
-          where: {
-            creator_id: isUserValid.id,
-            chatter_id: req.user.id,
-          },
-        });
-
-        if (firstDMList || secondDMList) {
-          req.flash("danger", "You have converstation with this user.");
-        } else {
-          await DMList.create({
-            creator_id: req.user.id,
-            chatter_id: isUserValid.id,
-            creator_name: req.user.username,
-            chatter_name: isUserValid.username,
-          });
-        }
-      } else {
-        req.flash("danger", "User is not defined");
-      }
-    } else {
-      req.flash("danger", "You can't start converstation to your self");
-    }
-    res.redirect(`/chat/${req.params.id}`);
-  } else {
-    console.log("req");
     await Message.create({
       chat_id: req.params.id,
       text: req.body.text,
@@ -97,6 +53,58 @@ const post = async (req, res) => {
       res.statusCode = 200;
       res.json({ sent: "ok", message: s.dataValues });
     });
+    return;
+  }
+
+  if (req.body.id == req.user.username) {
+    req.flash("danger", "You can't start converstation to your self");
+    return res.redirect("/chat/new");
+  }
+
+  const firstDMList = await DMList.findOne({
+    where: {
+      creator_id: req.user.id,
+      chatter_id: isUserValid.id,
+    },
+  });
+
+  const secondDMList = await DMList.findOne({
+    where: {
+      creator_id: isUserValid.id,
+      chatter_id: req.user.id,
+    },
+  });
+
+  if (firstDMList || secondDMList) {
+    req.flash("danger", "You have converstation with this user.");
+    return res.redirect("/chat/new");
+  }
+
+  const isUserValid = await User.findOne({
+    where: {
+      username: req.body.id,
+    },
+  });
+
+  if (isUserValid) {
+    let password;
+
+    req.body.encryptMsg == undefined
+      ? (password = false)
+      : (password = req.body.password);
+
+    await DMList.create({
+      creator_id: req.user.id,
+      chatter_id: isUserValid.id,
+      creator_name: req.user.username,
+      chatter_name: isUserValid.username,
+      password,
+    }).then((s) => {
+      res.redirect(`/chat/${s.dataValues.id}`);
+    });
+  } else {
+    req.flash("danger", "User is not defined");
+    res.redirect("/chat/new");
   }
 };
 
