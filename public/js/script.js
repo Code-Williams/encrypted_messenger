a289f333ebdefc3ca5d18f0aa91fe2cb66830412
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-10-05 03:59:07 +0330

    Message system completed

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,52 @@
-const socket = io();
+const socket = io("http://localhost:80");
+const sendContainer = document.getElementById("text_send");
+const textInput = document.getElementById("text_input");
+let roomOrg = window.location.href.split("/");
+let room = roomOrg[roomOrg.length - 1];
+
+if (roomOrg.includes("chat")) {
+  socket.emit("joinRoom", {
+    username: "User",
+    room,
+  });
+}
+
+textInput.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") btn.click();
+});
+
+sendContainer.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const message = textInput.value;
+
+  await axios.get("/userInfo").then((u) => {
+    axios
+      .post("/newMessage", {
+        chat_id: room,
+        sender_id: u.data.id,
+      })
+      .then((msg) => {
+        appenedMessage({ id: msg.data.id, message }, "right");
+
+        socket.emit("updateMessage", { id: msg.data.id, message });
+        socket.emit("messageCreate", {
+          message,
+          id: msg.data.id,
+          username: u.data.username,
+          room,
+        });
+      });
+  });
+
+  textInput.value = "";
+});
 
 socket.on("message", (data) => {
+  appenedMessage(data, "left");
+});
+
+function appenedMessage(data, side) {
   var new_li = document.createElement("li");
   var new_div = document.createElement("div");
   var username_p = document.createElement("p");
@@ -8,7 +54,7 @@ socket.on("message", (data) => {
   var messages = document.getElementById("messages");
 
   new_li.className = "message";
-  new_div.className = "sub-message left-side";
+  new_div.className = `sub-message ${side}-side`;
   username_p.className = "message-username";
   message_p.className = "message-message";
 
@@ -19,8 +65,8 @@ socket.on("message", (data) => {
 
   new_li.id = data.id;
 
-  username_p.innerHTML = data.username;
+  username_p.innerHTML = "You";
   message_p.innerHTML = data.message;
 
   new_li.scrollIntoView();
-});
+}
